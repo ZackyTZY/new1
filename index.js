a463697191b3e8a089b8fe6e5408c4ac3be3da14
Radit10 <85211622+Radit10@users.noreply.github.com>
2022-10-09 16:43:28 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -217,7 +217,7 @@ module.exports = alpha = async (alpha, m, chatUpdate, store, reSize) => {
         
         //punya gw
 		const reply = async (teks) => {
-			return alpha.sendMessage(m.chat, { text: teks, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `Selamat ${salam} ${pushname}`, body: `${ownername}`, previewType: "PHOTO", thumbnailUrl: ``, thumbnail: pp_bot, sourceUrl: `${myweb}`}}}, { quoted: m})			
+			return alpha.sendMessage(m.chat, { text: teks, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `Selamat ${salam} ${pushname}`, body: `${ownername}`, previewType: "PHOTO", thumbnailUrl: ``, thumbnail: pp_bot, sourceUrl: `${myweb}`}}}, { quoted: m})
 		}
 		const sendAudio = async (url) => {
 		    return alpha.sendMessage(m.chat, { audio: { url: url }, mimetype: 'audio/mp4', ptt: true, contextInfo:{ externalAdReply: { showAdAttribution: true, title: `Contoh Anak Kontol ${pushname}`, body: `${ownername}`, previewType: "PHOTO", thumbnailUrl: ppuser, thumbnail: '', sourceUrl: `${myweb}`}}}, {quoted:m})
@@ -239,6 +239,9 @@ module.exports = alpha = async (alpha, m, chatUpdate, store, reSize) => {
         }
         const deleteChat = (dari) => {
             return alpha.sendMessage(dari, { delete: { remoteJid: m.chat, fromMe: true, id: m.id, participant: m.sender } })
+        }
+        const hidetag = (teks) => {
+            return alpha.sendMessage(from, { text : teks ? teks : '' , mentions: participants.map(a => a.id)}, {quoted: fkontak})
         }
         const isEmoji = (emo) => {
             let emoji_ranges = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
@@ -1596,6 +1599,7 @@ break
             case 'hidetag':
                 if (!m.isGroup) return reply(lang.groupOnly())
                 if (!(isGroupAdmins || isGroupOwner )) return reply(lang.adminOnly())
+                if (isKecuali) return hidetag(`@${sender.split('@')[0]} Suka coli pake foto Yesus`)
                 alpha.sendMessage(from, { text : q ? q : '' , mentions: participants.map(a => a.id)}, {quoted: fkontak})
             break
 			case 'banned': {
